940ddd89a31c35b456a5229adb78a1b19b023ed2
Theresa Barkasy <112668448+thatreligionmajor@users.noreply.github.com>
2023-07-06 01:02:45 +0000

    complete, save for the gradient and header clashing

--- src/app.js
+++ src/app.js
@@ -1,11 +1,38 @@
 /* eslint-disable */
 import "bootstrap";
-import "./style.css";
+import "./styles.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  let who;
+//
+
+window.onclick = function() {
+  //elements of excuse
+  let subject = ["the dog", "my grandma", "his turtle", "my bird"];
+  let verb = ["ate", "spilled coffee on", "crushed", "broke"];
+  let object = ["my homework", "the keys", "the car"];
+  let dependentClause = [
+    "before class.",
+    "right on time.",
+    "when I finished.",
+    "during my lunch.",
+    "while I was praying."
+  ];
+  //random element in an array
+  let randomSubject = Math.floor(Math.random() * subject.length);
+  let randomVerb = Math.floor(Math.random() * verb.length);
+  let randomObject = Math.floor(Math.random() * object.length);
+  let randomDependentClause = Math.floor(
+    Math.random() * dependentClause.length
+  );
+  //call the function onload and set the excuse to innerHTML
+  document.getElementById("excuse").innerHTML =
+    subject[randomSubject] +
+    " " +
+    verb[randomVerb] +
+    " " +
+    object[randomObject] +
+    " " +
+    dependentClause[randomDependentClause];
 };
